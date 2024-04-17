fee0aab02360c85c8a6cbc7da793e25a9b9a201e
Ryan <116501522+p1poca2F3136@users.noreply.github.com>
2024-04-17 18:31:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,102 +1,88 @@
+import random
 
 def menu():
-    clases = ["1. Arqueiro", "2. Feiticeiro", "3. Cavaleiro"]
-    for item in clases:
+    classes = ["1. Arqueiro", "2. Feiticeiro", "3. Cavaleiro"]
+    for item in classes:
         print(item)
 
 def escolher_arco():
     arcos = ["1. arco de fogo", "2. arco de gelo", "3. arco encantado"]
     for item in arcos:
         print(item)
+    return arcos
 
 def escolher_feitico():
     feiticos = ["1. atordoamento", "2. envenenamento", "3. invocar zumbies"]
     for item in feiticos:
         print(item)
+    return feiticos
 
 def escolher_cavalo():
     cavalos = ["1. cavalo zumbie", "2. cavalo sombrio", "3. cavalo nautico"]
     for item in cavalos:
         print(item)
+    return cavalos
 
+def batalha():
+    jogador_vida = 100
+    inimigo_vida = random.randint(50, 100)
+    
+    print("Batalha iniciada!")
+    
+    while jogador_vida > 0 and inimigo_vida > 0:
+        print("Sua vida:", jogador_vida)
+        print("Vida do inimigo:", inimigo_vida)
+        print("1. Atacar")
+        print("2. Fugir")
+        escolha = int(input("Escolha sua ação: "))
+        
+        if escolha == 1:
+            dano_jogador = random.randint(5, 20)
+            dano_inimigo = random.randint(5, 15)
+            
+            jogador_vida -= dano_inimigo
+            inimigo_vida -= dano_jogador
+            
+            print("Você causou {} de dano ao inimigo.".format(dano_jogador))
+            print("O inimigo causou {} de dano a você.".format(dano_inimigo))
+        elif escolha == 2:
+            print("Você fugiu da batalha!")
+            break
+    
+    if jogador_vida <= 0:
+        print("Você foi derrotado!")
+    elif inimigo_vida <= 0:
+        print("Você venceu a batalha!")
 
 print("----------------")
 print("| RPG INICIADO |")
 print("________________")
 print()
 
-
-
-print("Escolha uma das clases a seguir: ")
-
+print("Escolha uma das classes a seguir: ")
 menu()
-
 print()
 
-escolher_clase = int(input("Escolher: "))
+escolher_classe = int(input("Escolher: "))
 
-clases_printar = ["1. Arqueiro", "2. Feiticeiro", "3. Cavaleiro"]
-
-if escolher_clase == 1:
-    print("Voce escolheu a clase: {}".format(clases_printar[0]))
-    print()
-
+if escolher_classe == 1:
+    print("Você escolheu a classe: Arqueiro")
     print("Escolha uma arma de arquearia:")
-    arcos_printar = ["1. arco de fogo", "2. arco de gelo", "3. arco encantado"]
-    escolher_arco()
-    print()
+    arco_escolhido = escolher_arco()
     escolher_arco = int(input("Escolher: "))
-
-    if escolher_arco == 1:
-        print("Voce escolheu o arco de: {} ".format(arcos_printar[0]))
-
-    elif escolher_arco == 2:
-        print("Voce escolheu o arco de: {}".format(arcos_printar[1]))
-
-    elif escolher_arco == 3:
-        print("Voce escolheu o arco de: {}".format(arcos_printar[2]))
-
-
-
-
-elif escolher_clase == 2:
-    print("Voce escolheu a clase: {}".format(clases_printar[1]))
-    print()
-
-    print("Escolha um tipo de feitico:")
-    feiticos_printar = ["1. atordoamento", "2. envenenamento", "3. invocar zumbies"]
-    escolher_feitico()
-    print()
+    print("Você escolheu o arco de: {}".format(arco_escolhido[escolher_arco - 1]))
+    batalha()
+elif escolher_classe == 2:
+    print("Você escolheu a classe: Feiticeiro")
+    print("Escolha um tipo de feitiço:")
+    feitico_escolhido = escolher_feitico()
     escolher_feitico = int(input("Escolher: "))
-
-    if  escolher_feitico == 1:
-        print("Voce escolheu o feitico de: {}".format(feiticos_printar[0]))
-
-    elif escolher_feitico == 2:
-        print("Voce escolheu o feitico de: {}".format(feiticos_printar[1]))
-
-    elif escolher_feitico == 3:
-        print("Voce escolheu o feitico de: {}".format(feiticos_printar[2]))
-
-
-
-
-elif escolher_clase == 3:
-    print("Voce escolheu a clase: {}".format(clases_printar[2]))
-    print()
-
+    print("Você escolheu o feitiço de: {}".format(feitico_escolhido[escolher_feitico - 1]))
+    batalha()
+elif escolher_classe == 3:
+    print("Você escolheu a classe: Cavaleiro")
     print("Escolha um tipo de cavalo:")
-    cavalos_printar = ["1. cavalo zumbie", "2. cavalo sombrio", "3. cavalo nautico"]
-    escolher_cavalo()
-    print()
+    cavalo_escolhido = escolher_cavalo()
     escolher_cavalo = int(input("Escolher: "))
-
-    if escolher_cavalo == 1:
-        print("Voce escolheu o cavalo: {}".format(cavalos_printar[0]))
-
-    elif escolher_cavalo == 2:
-        print("Voce escolheu o cavalo: {}".format(cavalos_printar[1]))
-
-    elif escolher_cavalo == 3:
-        print("Voce escolheu o cavalo: {}".format(cavalos_printar[2]))
-
+    print("Você escolheu o cavalo: {}".format(cavalo_escolhido[escolher_cavalo - 1]))
+    batalha()
